42080b3afaaea3f058c2b220fdd33f76982dde7f
Fatbardh <fatbardh@gmail.com>
2021-07-14 11:03:34 +0200

    reading state from store, dispatching action and passing the store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 
+import './index.css'
+import App from './App';
 import store from './store'
 
-// console.log('Initial state: ', store.getState())
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-
-console.log('State after dispatch: ', store.getState())
-// log: {todos: [...], filters: {status, colors}, meaningOfLife: 42}
-
-const unsubscribe = store.subscribe(() =>
-  console.log('State after dispatch: ', store.getState())
-)
-
-unsubscribe()
 
 ReactDOM.render(
-      <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
