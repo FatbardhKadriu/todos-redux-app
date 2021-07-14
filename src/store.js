656da1df347d864f46ac670c453d4deda42942c1
Fatbardh <fatbardh@gmail.com>
2021-07-14 09:55:46 +0200

    Adding the DevTools to the Store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer';
 import { sayHiOnDispatch, includeMeaningOfLife} from './exampleAddons/enhancers'
 import { print1, print2, print3 } from './exampleAddons/middleware';
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3);
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife);
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(print1, print2, print3),
+    sayHiOnDispatch, 
+    includeMeaningOfLife);
 
-const store = createStore(rootReducer, middlewareEnhancer);
+const store = createStore(rootReducer, composedEnhancer);
 
 export default store;
